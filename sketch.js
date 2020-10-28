387ed232971009610cdbde9dd6c043de0eea6f7a
Sharon Robins <sharon.robbin.120026@standrewsindia.com>
2020-10-28 18:53:11 +0530

    constrained bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constrainedLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -15,7 +17,7 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
+    
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
@@ -28,13 +30,15 @@ function setup(){
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
 
-    log3 =  new Log(810,180,300, PI/2);
+    log2 =  new Log(810,180,300, PI/2);
 
     box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
+    log3 = new Log(760,120,150, PI/7);
+    log4 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    log5 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log5.body)
 
 }
 
@@ -53,12 +57,15 @@ function draw(){
     box3.display();
     box4.display();
     pig3.display();
-    log3.display();
+    log2.display();
 
     box5.display();
+    log3.display();
     log4.display();
-    log5.display();
 
     bird.display();
     platform.display();
+    log5.display();
+    chain.display();
+    
 }
